620e3b7e1d111d725a14b5b1eb4a346f799a1918
한결 <hgsij@knou.ac.kr>
2024-11-10 22:36:30 +0900

    백준5-8

---
+++ src/main/java/Baekjoon/Step_5_8.java
@@ -0,0 +1,10 @@
+package Baekjoon;
+import java.util.*;
+public class Step_5_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(input," ");
+        System.out.println(st.countTokens());
+    }
+}
