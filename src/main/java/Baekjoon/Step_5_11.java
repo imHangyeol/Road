ff83d31f5be9a4be5082c728f4acc86fbd7ba832
한결 <hgsij@knou.ac.kr>
2024-11-13 11:55:07 +0900

    백준5-11

---
+++ src/main/java/Baekjoon/Step_5_11.java
@@ -0,0 +1,11 @@
+package Baekjoon;
+import java.util.*;
+public class Step_5_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            System.out.println(input);
+        }
+    }
+}
