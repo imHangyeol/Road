b66528ed5a3c5c16ce7b6aa41dffa13311196fcb
한결 <hgsij@knou.ac.kr>
2024-11-23 20:50:15 +0900

    백준 3-2

---
+++ src/main/java/Baekjoon/Step_3_2.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class Step_3_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int j = sc.nextInt();
+
+
+
+        for(int i=1; i <= j; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
